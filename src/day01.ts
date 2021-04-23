f1fd8c3cd25d4ae09216b1c281e2ac893d3783c0
Gareth Spring <gareth.spring@dunnhumby.com>
2021-04-23 15:13:01 +0100

    Add jest tests for day 1

--- src/day01.ts
+++ src/day01.ts
@@ -9,7 +9,7 @@ function readInput() : string {
     return fs.readFileSync(filePath, 'utf-8');
 }
 
-function Part1() : number {
+export function Part1() : number {
     let input = readInput();
     let digits = new Array();
 
@@ -30,7 +30,7 @@ function Part1() : number {
          return a + b });
 }
 
-function Part2() : number {
+export function Part2() : number {
     let input = readInput();
     let digits = new Array();
 
